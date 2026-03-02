a6c75e831724e7e4ed6dee53bb0e329c827138f3
fog <20683340+dreamboy1339@users.noreply.github.com>
2026-03-02 11:44:15 +0900

    wip: add new package structure and initial classes

---
+++ src/test/java/com/asdf/minilog/controller/FeedControllerTest.java
@@ -0,0 +1,7 @@
+package com.asdf.minilog.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeedControllerTest {
+
+}
---
+++ src/test/java/com/asdf/minilog/service/ArticleServiceTest.java
@@ -0,0 +1,7 @@
+package com.asdf.minilog.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleServiceTest {
+
+}
