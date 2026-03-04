24ab1e1cdb6feeeb452299fc7b56695b2aa55a91
fog <20683340+dreamboy1339@users.noreply.github.com>
2026-03-04 20:59:05 +0900

    chore: add custom exception classes and global exception handler with API responses

--- src/main/java/com/asdf/minilog/exception/UserNotFoundException.java
+++ src/main/java/com/asdf/minilog/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package com.asdf.minilog.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
