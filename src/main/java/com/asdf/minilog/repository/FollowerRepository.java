c632e1f8a902ff88082fc39aa3b4bfe3e3edd7c4
fog <20683340+dreamboy1339@users.noreply.github.com>
2026-03-05 21:36:38 +0900

    chore: replace FollowerRepository implementation with Spring Data JPA interface

--- src/main/java/com/asdf/minilog/repository/FollowerRepository.java
+++ src/main/java/com/asdf/minilog/repository/FollowerRepository.java
@@ -1,4 +1,13 @@
 package com.asdf.minilog.repository;
 
-public class FollowerRepository {
+import com.asdf.minilog.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowerRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findByFolloweeId(Long followeeId);
+
+    Optional<Follow> findByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
 }
