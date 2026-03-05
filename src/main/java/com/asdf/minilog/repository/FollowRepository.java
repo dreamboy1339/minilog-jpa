17836fc220d17baca196f81d5996d3395f9370d7
fog <20683340+dreamboy1339@users.noreply.github.com>
2026-03-05 22:47:19 +0900

    chore: rename FollowerRepository to FollowRepository and implement FollowService with follow, unfollow, and list retrieval operations

--- src/main/java/com/asdf/minilog/repository/FollowRepository.java
+++ src/main/java/com/asdf/minilog/repository/FollowRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface FollowerRepository extends JpaRepository<Follow, Long> {
-    List<Follow> findByFolloweeId(Long followeeId);
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findByFollowerId(Long followeeId);
 
     Optional<Follow> findByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
 }
